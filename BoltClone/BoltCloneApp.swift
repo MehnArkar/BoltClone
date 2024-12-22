fb10929b8691a1e18b87e3017d7a19eee96026ce
Arkar <arkarmin.dev@gmail.com>
2024-12-22 22:47:43 +0700

    Initial Commit

---
+++ BoltClone/BoltCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoltCloneApp.swift
+//  BoltClone
+//
+//  Created by Arkar Min on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BoltCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoltCloneTests/BoltCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoltCloneTests.swift
+//  BoltCloneTests
+//
+//  Created by Arkar Min on 22/12/2024.
+//
+
+import Testing
+@testable import BoltClone
+
+struct BoltCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
